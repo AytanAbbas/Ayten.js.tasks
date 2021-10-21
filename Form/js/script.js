fc5b62671f82bc0ae33d82093208774bfdb6bebf
AytanAbbas <87373585+AytanAbbas@users.noreply.github.com>
2021-10-21 09:50:19 +0400

    Add files via upload

---
+++ Form/js/script.js
@@ -0,0 +1,10 @@
+function getValue() {
+    // document.querySelector("#textareaID").value;
+    // document.getElementById("myInput").value;
+    // document.getElementById("list").value;
+    console.log(document.getElementById("list").value);
+    console.log(document.getElementById("myInput").value);
+    console.log(document.getElementById("myInput1").value);
+    console.log(document.querySelector("#textareaID").value);
+
+}
